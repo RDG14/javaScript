9669370ee6153cc12353f221f018eec2cdaca5f5
Rodrigo Alex <rodrigomoreirasantosa@gmail.com>
2023-01-25 11:46:40 -0300

    Atualizado

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,10 @@
+let vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60) { //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+//Se a velocidade for maior do que 60 multado
+
+console.log('Dirija sempre usando cinto de segurança')
---
+++ exercicios/aula011/ex009.js
@@ -0,0 +1,8 @@
+let país = 'EUA'
+console.log(`Vivendo em ${país}`)
+
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}
